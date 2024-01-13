6229afaf73cd23a2782efb359b0a333c5a3331c5
Peeyush Singh <peeyushsrj@gmail.com>
2024-01-14 03:22:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var app = angular.module('myApp', []);
 
 app.controller('MyController', function($scope, $http) {
-    $http.get('https://api.fosting.in/')
+    $http.get('https://api.fosting.in/api')
         .then(function(response) {
             $scope.data = response.data;
         }, function(error) {
