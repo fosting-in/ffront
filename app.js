40b88920fa4a860c0bca31f9383babedbcb301b8
Peeyush Singh <peeyushsrj@gmail.com>
2024-01-14 03:38:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var url1 = "https://3k8nooux38.execute-api.us-east-1.amazonaws.com/prod"
 var url2 = 'https://api.fosting.in/api' 
 
 app.controller('MyController', function($scope, $http) {
-    $http.get(url1)
+    $http.get(url2)
         .then(function(response) {
             $scope.data = response.data;
         }, function(error) {
