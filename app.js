3207cc0656a8e07c74e8e1c156e6f591275d9f8c
Peeyush Singh <peeyushsrj@gmail.com>
2024-01-14 00:46:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var app = angular.module('myApp', []);
 
 app.controller('MyController', function($scope, $http) {
-    $http.get('https://newsapi.org/v2/everything?q=tesla&from=2023-12-13&sortBy=publishedAt&apiKey=API_KEY')
+    $http.get('https://api.fosting.in/')
         .then(function(response) {
             $scope.data = response.data;
         }, function(error) {
