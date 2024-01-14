e736d5d98fc76155716c026505b0b50fb7ddbe9d
Peeyush Singh <peeyushsrj@gmail.com>
2024-01-14 20:09:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 var app = angular.module('myApp', []);
 var url1 = "https://3k8nooux38.execute-api.us-east-1.amazonaws.com/prod"
-var url2 = 'https://api.fosting.in/api' 
+var url2 = 'https://api.fosting.in/api2' 
 
 var app = angular.module('myApp', []);
 
 app.controller('MyController', function($scope, $http) {
     $http({
         method: 'POST',
-        url: 'https://api.fosting.in/api',
-        withCredentials: false,
+        url: url2,
         data: {
             key1: "a",
             key2: "b",
             key3: "c"
         },
+        withCredentials: false,
     }).then(function successCallback(response) {
         console.log(response);
         $scope.x = response.data;
