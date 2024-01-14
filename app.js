0e85e6b427b86bad2f7554beec2e7f92fa3e7563
Peeyush Singh <peeyushsrj@gmail.com>
2024-01-14 17:08:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,17 +5,21 @@ var url2 = 'https://api.fosting.in/api'
 var app = angular.module('myApp', []);
 
 app.controller('MyController', function($scope, $http) {
-    var config = {
-        headers: {
-            'Access-Control-Allow-Origin': '*'
-        }
-    };
-
-    $http.get(url2, config)
-        .then(function(response) {
-            $scope.data = response.data;
-        }, function(error) {
-            console.error('Error fetching data:', error);
-        });
+    // var config = {
+    //     headers: {
+    //         'Access-Control-Allow-Origin': '*'
+    //     }
+    // };
+    // $http.get(url2, config)
+    //     .then(function(response) {
+    //         $scope.data = response.data;
+    //     }, function(error) {
+    //         console.error('Error fetching data:', error);
+    //     });
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "https://api.fosting.in/api", true);
+xhr.withCredentials = false;
+xhr.send(null);
+console.log(xhr)
 });
 
